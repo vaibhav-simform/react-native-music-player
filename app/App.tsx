6b22ff74829b491e1486a77e0204cc79dafee97b
vaibhav-simform <vaibhav.a@simformsolutions.com>
2023-06-29 11:08:25 +0530

    feat: TE1-T2806: Initial project setup

---
+++ app/App.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react';
+import {Text, View} from 'react-native';
+
+const App: FC = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
